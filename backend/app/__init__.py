479edaba7075e1250c18ee477964ac06bd17cd89
Shamerock13 <shamerock.mg@googlemail.com>
2026-07-26 19:28:23 +0200

    feat: register enrichment findings router

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,6 +3,8 @@ from . import research_source_models  # noqa: F401
 from .research_routes import router as research_router
 from .research_source_routes import router as research_source_router
 from .enrichment_routes import router as enrichment_router
+from .enrichment_finding_routes import router as enrichment_finding_router
 
 research_router.include_router(research_source_router)
 research_router.routes.extend(enrichment_router.routes)
+research_router.routes.extend(enrichment_finding_router.routes)
