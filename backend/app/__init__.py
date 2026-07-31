d7173f52065db813751bcfddd1b9b0213757b3ee
Shamerock13 <shamerock.mg@googlemail.com>
2026-07-31 20:04:54 +0200

    feat(prices): register price alert model and routes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,7 @@
 # Package initialization keeps feature routers composable without bloating main.py.
 from . import research_source_models  # noqa: F401
 from . import price_source_review_models  # noqa: F401
+from . import price_alert_models  # noqa: F401
 from .research_routes import router as research_router
 from .research_source_routes import router as research_source_router
 from .enrichment_routes import router as enrichment_router
@@ -12,6 +13,7 @@ from .twin_workflow_routes import router as twin_workflow_router
 from .targeted_research_routes import router as targeted_research_router
 from .brand_research_routes import router as brand_research_router
 from .price_routes import router as price_router
+from .price_alert_routes import router as price_alert_router
 from .price_seed_routes import router as price_seed_router
 from .price_scanner_routes import router as price_scanner_router
 from .price_discovery_routes import router as price_discovery_router
@@ -29,6 +31,7 @@ research_router.routes.extend(enrichment_router.routes)
 research_router.routes.extend(enrichment_finding_router.routes)
 research_router.routes.extend(research_enrichment_router.routes)
 research_router.routes.extend(price_router.routes)
+research_router.routes.extend(price_alert_router.routes)
 research_router.routes.extend(price_source_review_router.routes)
 research_router.routes.extend(price_scanner_router.routes)
 research_router.routes.extend(price_discovery_router.routes)
